8ad3e9c9205272109ce5bb55006114b23490edaa
fsluizvictor <fs.luizvictor@gmail.com>
2021-11-03 21:08:04 -0300

    add errors

--- main.py
+++ main.py
@@ -10,16 +10,15 @@ DIMENSION = 2
 COUNT_POINTS = 100
 COUNT_EPOCHS = 1000
 ADJUSTMENT = 1
+ALFA = 0.001
 
 # INITIALIZE ARRAYS
-y = [0] * COUNT_POINTS
 x = [random.uniform(-DIMENSION, DIMENSION) for i in range(COUNT_POINTS)]
 x.sort()
 
 
 def run():
     # TODO: add the plot of original function
-
     pass
 
 
@@ -35,8 +34,40 @@ def otimization():
     p2 = 1.8
     q2 = 0
 
+    # generate gaussians
     w1, w2 = gaussians_generate(x1, x2, sd1, sd2)
     plot(x, w1, x, w2)
+
+    w1_n = list()
+    w2_n = list()
+
+    for i in range(COUNT_POINTS):
+        w1_n = w1[i] / w1[i] + w2[i]
+        w2_n = w2[i] / w2[i] + w1[i]
+
+    y1 = list()
+    y2 = list()
+
+    for x_n in x:
+        y1.append(p1 * x_n + q1)
+        y2.append(p2 * x_n + q2)
+
+    yd = list()
+    for i in range(COUNT_POINTS):
+        yd.append(w1_n[i] * y1[i] - w2_n[i] * y2[i])
+
+    y = list()
+    for x_n in x:
+        y.append(x_n ** 2)
+
+    # calcule of initial error
+    error = list()
+    for i in range(COUNT_POINTS):
+        error.append(((y[i] - yd[i]) ** 2) / 2)
+
+    for i in range(COUNT_EPOCHS):
+        for p in range(COUNT_POINTS):
+            a = p
 
 
 def gradient(x1: float, sig1: float, x2: float, sig2: float, p1: float, p2: float, q1: float, q2: float, y: float,
@@ -54,10 +85,11 @@ def gradient(x1: float, sig1: float, x2: float, sig2: float, p1: float, p2: floa
 def gaussians_generate(x1: float, x2: float, sigma1: float, sigma2: float) -> tuple[List: float, List: float]:
     w1 = list()
     w2 = list()
-    for i in x:
-        w1.append(__gaussian(i, x1, sigma2))
-        w2.append(__gaussian(i, x2, sigma2))
-    return [w1, w2]
+
+    return w1, w2
+
+
+def curve_approximate_generate(w1: List[float], w2: List[float]) -> tuple[List: float, List: float]:
 
 
 def plot(array_x1: List[float], array_y1: List[float], array_x2: List[float], array_y2: List[float]):
