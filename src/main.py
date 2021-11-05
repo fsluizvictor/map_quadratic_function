08536d2c0e17b063b99abfd615569dbe9b6da025
fsluizvictor <fs.luizvictor@gmail.com>
2021-11-04 21:27:00 -0300

    add read-me and refactor the code

--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ ALFA = 0.001
 # INITIALIZE ARRAYS
 x = [random.uniform(-DIMENSION, DIMENSION) for i in range(COUNT_POINTS)]
 x.sort()
+y = [i ** 2 for i in x]
 
 
 def run():
@@ -22,52 +23,59 @@ def run():
     pass
 
 
-def otimization():
+def sugeno():
     # ENTRY VALUES
+    # in
     x1 = 2
     sd1 = 1.7
     x2 = -2
     sd2 = 1.7
 
+    # out
     p1 = -1.8
     q1 = 0
     p2 = 1.8
     q2 = 0
 
-    # generate gaussians
-    w1, w2 = gaussians_generate(x1, x2, sd1, sd2)
-    plot(x, w1, x, w2)
-
-    w1_n = list()
-    w2_n = list()
-
-    for i in range(COUNT_POINTS):
-        w1_n = w1[i] / w1[i] + w2[i]
-        w2_n = w2[i] / w2[i] + w1[i]
-
-    y1 = list()
-    y2 = list()
-
-    for x_n in x:
-        y1.append(p1 * x_n + q1)
-        y2.append(p2 * x_n + q2)
-
-    yd = list()
-    for i in range(COUNT_POINTS):
-        yd.append(w1_n[i] * y1[i] - w2_n[i] * y2[i])
-
-    y = list()
-    for x_n in x:
-        y.append(x_n ** 2)
-
-    # calcule of initial error
-    error = list()
-    for i in range(COUNT_POINTS):
-        error.append(((y[i] - yd[i]) ** 2) / 2)
+    # # generate gaussians
+    # w1, w2 = gaussians_generate(x1, x2, sd1, sd2)
+    # plot(x, w1, x, w2)
+    #
+    # w1_n = list()
+    # w2_n = list()
+    #
+    # for i in range(COUNT_POINTS):
+    #     w1_n = w1[i] / w1[i] + w2[i]
+    #     w2_n = w2[i] / w2[i] + w1[i]
+    #
+    # y1 = list()
+    # y2 = list()
+    #
+    # for x_n in x:
+    #     y1.append(p1 * x_n + q1)
+    #     y2.append(p2 * x_n + q2)
+    #
+    # yd = list()
+    # for i in range(COUNT_POINTS):
+    #     yd.append(w1_n[i] * y1[i] - w2_n[i] * y2[i])
+    #
+    # y = list()
+    # for x_n in x:
+    #     y.append(x_n ** 2)
+    #
+    # # calcule of initial error
+    # error = list()
+    # for i in range(COUNT_POINTS):
+    #     error.append(((y[i] - yd[i]) ** 2) / 2)
 
     for i in range(COUNT_EPOCHS):
-        for p in range(COUNT_POINTS):
-            a = p
+        random.shuffle(x)
+
+        for j in range(COUNT_POINTS):
+            alpha = 0.01
+            yd = x[j] ** 2
+
+            w1, w2 = gauss_m_f(x[j], x1, x2, sd1, sd2)
 
 
 def gradient(x1: float, sig1: float, x2: float, sig2: float, p1: float, p2: float, q1: float, q2: float, y: float,
@@ -82,13 +90,10 @@ def gradient(x1: float, sig1: float, x2: float, sig2: float, p1: float, p2: floa
     y2 = pk2 * x1 + qk2
 
 
-def gaussians_generate(x1: float, x2: float, sigma1: float, sigma2: float) -> tuple[List: float, List: float]:
-    w1 = list()
-    w2 = list()
-    for i in x:
-        w1.append(__gaussian(i, x1, sigma2))
-        w2.append(__gaussian(i, x2, sigma2))
-    return [w1, w2]
+def gauss_m_f(x: float, x1: float, x2: float, sigma1: float, sigma2: float) -> tuple[float, float]:
+    w1 = __gaussian_membership_function(x, x1, sigma1)
+    w2 = __gaussian_membership_function(x, x2, sigma2)
+    return w1, w2
 
 
 def curve_approximate_generate(w1: List[float], w2: List[float]) -> tuple[List: float, List: float]:
@@ -122,9 +127,9 @@ def __y(y1: float, y2: float, w1: float, w2: float):
     return (w1 * y1 + w2 * y2) / w1 + w2
 
 
-def __gaussian(x: float, mu: float, sigma: float) -> float:
+def __gaussian_membership_function(x: float, mu: float, sigma: float) -> float:
     return math.exp(-1 / 2 * ((x - mu) / sigma) ** 2)
 
 
 if __name__ == '__main__':
-    otimization()
+    sugeno()
